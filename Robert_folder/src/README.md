67b225ea1c9739b35da5df85ee165dcefc098055
Zain Mirza <zainmirza0824@gmail.com>
2022-12-12 20:31:14 -0800

    adding report

---
+++ Robert_folder/src/README.md
@@ -0,0 +1,60 @@
+The Python script `facts.py` show us different facts about the dataset of Soccer.
+To launch this code, use the command `python3 facts.py`.
+This dataset show us all match between 1993 and 2022.
+We can see the date, home team and the away team, where they are from, who win, the score and the score of players.
+
+This is what we get when we launch the code:
+`Team that scored the most goals at Home is: USA with 589 goals`,
+`Team that scored the most goals Away is: Brazil with 396 goals`,
+`Team that scored the most goals Overall is: Brazil with 954 goals`,
+
+`Team that have the highest goals per match at Home is: New Caledonia with 2.6785714285714284 goals`,
+`Team that have the highest goals per match Away is: Vanuatu with 2.314814814814815 goals`,
+`Team that have the highest goals per match overall is: New Caledonia with 2.2582046332046333 goals`,
+
+`The team who have to most clean sheet at Home is: USA with 152 clean sheets`,
+`This is 48.40764331210191 % clean sheets`,
+`The team who have to most clean sheet Away is: Zambia with 96 clean sheets`,
+`This is 39.50617283950617 % clean sheets`,
+`The team who have to most clean sheet overall is: Brazil with 229 clean sheets`,
+`This is 52.88683602771363 % clean sheets`,
+
+`The team at home have: 49.166004765687056 % chances to win`,
+`The away team have: 28.305672839764224 % chances to win`,
+`There is 22.528322394548724 % chances to have a draw`,
+
+`The team who won the most of match is: USA with 194 victory at Home which is 61.78343949044586 % victory`
+`The team who won the most of match is: Brazil with 120 victory Away which is 60.0 % victory`
+`The team who won the most of match is: Brazil with 301 victory Overall which is 69.5150115473441 % victory`
+
+**1 Who scored the more goals ?**
+
+In the first part, I wanted to know which team scored the more goals at Home, Away and Overall.
+The USA scored 589 goals at Home, Away it’s Brazil with 396 goals and overall the team who score the more goals is Brazil with 954 goals.
+
+To find this results, I used Pandas with `groupby` to group all home and away teams and then I use `.sum()` to take all goals. Then I used the function `max()` to know who scored the more goals.
+
+**2 Who have the more goals per game ?**
+
+In this part I wanted to know if the teams who scored the more goals scored because they score many goals pre matches. As we can see, it’s not the case. It’s New Caledonia who scores the more goals pre match at Home and Overall with approximately 2.25 goals per match.
+
+For this fact, I used the same code as before for the part one but instead of `sum()` I used `mean()`.
+
+**3 Who have the more Clean Sheet**
+
+A clean Sheet means that one team kept the opposing team from scoring a goal so 0 goal. I wanted to know if a good team who won games it’s not only about scoring goals but also having a good defense. At Home the USA have more than 48% clean sheets for all their games. Away it’s Zambia and overall it’s Brazil with more that 52% clean sheets.
+
+For this part I filtered data to say that I only want data with teams who scored zero goal.
+
+**4 Who have the more chance to win between a team at Home or away ?**
+
+For this part I was thinking that it can be interesting to know if the concept of game at Home or Away really can be important. As we can see, a team at home have approximately 1/2 to win a game ! This is a huge advantage for the Home team, the away team have 1/4 chances to lose or to have at least a draw.
+So yes playing at Home or playing Away can strongly impact the final score.
+
+For this part I used the filtration that I want only to see the count of game that was won by a Home team of not.
+
+**5 Who is the Best ?**
+
+The part that everyone wants to know, who is the Best ? Who won the more games ? At Home it’s USA who won more that 61% of their games but then Brazil is too strong Away and Overall. Thanks to the part 1 and 3, we can see something really interesting that USA is the best team to score at Home and also the best defense at Home, this explains easily why they win many games and we can see the same things with Brazil, best team to score and best defense so this explains why they won many matches.
+
+Here also I filtered data to have only the count of how much a team Won.
---
+++ Robert_folder/src/facts.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python3
+## SFSU, 2022
+## Robert Harakaly
+## File description:
+## GroupProject, Fifa WorldCup
+##
+
+import pandas as pd
+import numpy as np
+from math import *
+from collections import Counter
+import matplotlib.pyplot as plt
+
+file = "../doc/sport/fifa_world_cup.csv"
+data = pd.read_csv(file)
+
+
+def max_goal(type):
+    if type == "sum":
+        most_goal = pd.DataFrame(data.groupby(['home_team']).sum())
+        most_goal_a = pd.DataFrame(data.groupby(['away_team']).sum())
+    if type == "mean":
+        most_goal = pd.DataFrame(data.groupby(['home_team']).mean())
+        most_goal_a = pd.DataFrame(data.groupby(['away_team']).mean())
+    most_goal = pd.DataFrame(most_goal.iloc[0:, [4]]).to_numpy()
+    most_goal_a = pd.DataFrame(most_goal_a.iloc[0:, [5]]).to_numpy()
+    tot = []
+    for idx, i in enumerate(most_goal_a):
+        if type == "sum":
+            tot.append(int(i+most_goal[idx]))
+        if type == "mean":
+            tot.append(float((i+most_goal[idx])/2))
+    country = data.groupby('home_team')['home_team'].apply(list)
+    c = []
+    for idx, i in enumerate(country):
+        c.append(i[0])
+    m_h = max(most_goal)
+    m_a = max(most_goal_a)
+    m = max(tot)
+    j = 0
+    j_h = 0
+    j_a = 0
+    for idx, i in enumerate(c):
+        if tot[idx] == m:
+            j = idx
+        if most_goal[idx] == m_h:
+            j_h = idx
+        if most_goal_a[idx] == m_a:
+            j_a = idx
+    # if type == "sum":
+    #     print("Team that scored the most goals at Home is:", c[j_h], "with", m_h[0], "goals")
+    #     print("Team that scored the most goals Away is:", c[j_a], "with", m_a[0], "goals")
+    #     print("Team that scored the most goals Overall is:", c[j], "with", m, "goals")
+    # if type == "mean":
+    #     print("Team that have the highest goals per match at Home is:", c[j_h], "with", m_h[0], "goals")
+    #     print("Team that have the highest goals per match Away is:", c[j_a], "with", m_a[0], "goals")
+    #     print("Team that have the highest goals per match overall is:", c[j], "with", m, "goals")
+    return c
+
+def who_scored_the_most_goals():
+    c = max_goal("sum")
+#    print()
+    max_goal("mean")
+    return c
+
+def home_clean_sheet(home, team):
+    clean_sheet = pd.DataFrame(data[data[home] == 0])
+    clean_sheet = clean_sheet[team].to_numpy()
+    cs = []
+    for i in clean_sheet:
+        cs.append(i)
+    a = dict(Counter(cs))
+    t = max(a, key=a.get)
+    if team == 'home_team':
+        tot_game = len(data[data['home_team'] == t])
+#        print("The team who have to most clean sheet at Home is:", t, "with", a[t], "clean sheets")
+#        print("This is", (a[t]/tot_game)*100, "% clean sheets")
+    if team == 'away_team':
+        tot_game = len(data[data['away_team'] == t])
+##        print("The team who have to most clean sheet Away is:", t, "with", a[t], "clean sheets")
+##        print("This is", (a[t]/tot_game)*100, "% clean sheets")
+    return a, cs, tot_game
+
+def clean_sheet(c):
+    a_h, cs, tot_game_h = home_clean_sheet("away_team_score", "home_team")
+    a_a, cs, tot_game_a = home_clean_sheet("home_team_score", "away_team")
+    tot = []
+#    tot_game = tot_game_h + tot_game_a
+    for idx, i in enumerate(cs):
+        try:
+            tot.append(a_h[i]+a_a[i])
+        except:
+            continue
+    m = max(tot)
+    j = 0
+    for idx, i in enumerate(cs):
+        if m == tot[idx]:
+            j = idx
+            break
+    tot_game = len(data[(data['home_team'] == cs[j]) | (data['away_team'] == cs[j])])
+#    print("The team who have to most clean sheet overall is:", cs[j], "with", m, "clean sheets")
+#    print("This is", (m/tot_game)*100, "% clean sheets")
+
+def Home_or_Away():
+    global data
+#    print()
+    win = (len(data[data['home_team_result'] == 'Win'])/(len(data))*100)
+    lose = (len(data[data['home_team_result'] == 'Lose'])/(len(data))*100)
+    draw = (len(data[data['home_team_result'] == 'Draw'])/(len(data))*100)
+##    print("The team at home have:", win, "% chances to win")
+#    print("The away team have:", lose, "% chances to win")
+#    print("There is", draw, "% chances to have a draw")
+
+def who_win_the_most(c, team, score, data):
+#    global data
+    win = []
+    for i in c:
+        win.append(len(data[(data[team] == i) & (data['home_team_result'] == score)]))
+    m = max(win)
+    j = 0
+    for idx, i in enumerate(win):
+        if m == i:
+            j = idx
+    tot_game = len(data[data[team] == c[j]])
+    # if team == 'home_team':
+    #     print("The team who won the most of match is:", c[j], "with", m, "victory at Home which is", m/tot_game*100, "% victory")
+    # if team == 'away_team':
+    #     print("The team who won the most of match is:", c[j], "with", m, "victory Away which is", m/tot_game*100, "% victory")
+    return win
+
+
+def best_country(c, data):
+    win_h = who_win_the_most(c, 'home_team', "Win", data)
+    win_a = who_win_the_most(c, 'away_team', "Lose", data)
+    lose_h = who_win_the_most(c, 'home_team', "Lose", data)
+    lose_a = who_win_the_most(c, 'away_team', "Win", data)
+    draw_h = who_win_the_most(c, 'home_team', "Draw", data)
+    draw_a = who_win_the_most(c, 'away_team', "Draw", data)
+    win = []
+    lose = []
+    draw = []
+    for idx, i in enumerate(win_h):
+        win.append(i + win_a[idx])
+        lose.append(lose_h[idx] + lose_a[idx])
+        draw.append(draw_h[idx] + draw_a[idx])
+    m = max(win)
+    j = 0
+    for idx, i in enumerate(win):
+        if m == i:
+            j = idx
+    tot_game = len(data[(data['home_team'] == c[j]) | (data['away_team'] == c[j])])
+#    print("The team who won the most of match is:", c[j], "with", m, "victory Overall which is", m/tot_game*100, "% victory")
+    return win, lose, draw
+
+def data_for_a_certain_y(y, data):
+#    global data
+    for idx, d in enumerate(data["date"]):
+        if str(2022-y) == d[:4]:
+            pastYearsData = data.iloc[1+idx:, 0:]
+#    print(pastYearsData)
+    return pastYearsData
+
+def new_df(c, w, l, d, df, bl, y):
+    percent_w = []
+    percent_l = []
+    percent_d = []
+    tot = []
+    for idx, i in enumerate(c):
+        tot.append(w[idx]+l[idx]+d[idx])
+        try:
+            percent_w.append(w[idx]/(tot[idx])*100)
+            percent_l.append(l[idx]/(tot[idx])*100)
+            percent_d.append(d[idx]/(tot[idx])*100)
+        except:
+            percent_w.append("NaN")
+            percent_l.append("NaN")
+            percent_d.append("NaN")
+    if bl == False:
+        data2 = {'TEAM': c,
+        'WIN': w,
+        'LOSE': l,
+        'DRAW': d,
+        'PLAY': tot
+#            '% WIN': percent_w, '% LOSE': percent_l, '% DRAW': percent_d
+        }
+        df = pd.DataFrame(data2)
+    else:
+        df["last " + str(y) + "y WIN"] = w
+        df["last " + str(y) + "y LOSE"] = l
+        df["last " + str(y) + "y DRAW"] = d
+        df["last " + str(y) + "y PLAY"] = tot
+        # df["last " + str(y) + "y % WIN"] = percent_w
+        # df["last " + str(y) + "y % LOSE"] = percent_l
+        # df["last " + str(y) + "y % DRAW"] = percent_d
+    return df
+
+GroupA = {"Qatar":0, "Ecuador":0, "Senegal":0, "Netherlands":0}
+GroupB = {"England":0, "IR Iran":0, "USA":0, "Wales":0}
+GroupC = {"Argentina":0, "Saudi Arabia":0, "Mexico":0, "Poland":0}
+GroupD = {"Australia":0, "France":0, "Denmark":0, "Tunisia":0}
+GroupE = {"Spain":0, "Germany":0, "Japan":0, "Costa Rica":0}
+GroupF = {"Belgium":0, "Canada":0, "Morocco":0, "Croatia":0}
+GroupG = {"Brazil":0, "Serbia":0, "Switzerland":0, "Cameroon":0}
+GroupH = {"Portugal":0, "Ghana":0, "Uruguay":0, "Korea Republic":0}
+
+GROUP = [GroupA, GroupB, GroupC, GroupD, GroupE, GroupF, GroupG, GroupH]
+
+def rank(team1 = None, team2 = None):
+    r = data.loc[:len(data), ['home_team', 'away_team', 'home_team_fifa_rank', 'away_team_fifa_rank', 'home_team_result']]
+    bins = [0, 10, 30, 40, 70, 100, 211]
+    ranking = ["World class", "Really Good", "Average", "Bad", "Really Bad", "Worst"]
+    r["home ranking"] = pd.cut(x=data['home_team_fifa_rank'], bins=bins, labels=ranking)
+    r["away ranking"] = pd.cut(x=data['away_team_fifa_rank'], bins=bins, labels=ranking)
+    rr = r.loc[:len(r), ['home ranking', 'away ranking', 'home_team_result']]
+    win_rate = []
+    for g1 in ranking:
+        for g2 in ranking:
+            game = rr[((rr["home ranking"] == g1) & (rr["away ranking"] == g2))]
+            nb_games = len(rr[((rr["home ranking"] == g1) & (rr["away ranking"] == g2))])
+            for g in ["Win", "Lose", "Draw"]:
+                if team1 != None and team2 != None and team1 == g1 and team2 == g2 or team1 == None and team2 == None:
+                    win_rate.append(len(game[game["home_team_result"] == g]) / nb_games * 100)
+#                    print(g, "HOME:", g1, "against AWAY:", g2, "\t", (len(game[game["home_team_result"] == g]) / nb_games * 100))
+            if team1 != None and team2 != None and team1 == g1 and team2 == g2:
+                return win_rate
+#            print()
+
+def main():
+#    print()
+    c = who_scored_the_most_goals()
+#    print()
+    clean_sheet(c)
+    Home_or_Away()
+#    print()
+    # for t in GROUP:
+    #     for tt in t:
+    #         WorldCupData = data[(data['home_team'] == tt) | (data['away_team'] == tt)]
+    # print("ONLY WORLD CUP TEAMS\n", WorldCupData)
+    w, l, d = best_country(c, data)
+    df = new_df(c, w, l, d, None, False, 0)
+    pastYearsData = data_for_a_certain_y(5, data)
+    p_w, p_l, p_d = best_country(c, pastYearsData)
+    df = new_df(c, p_w, p_l, p_d, df, True, 5)
+    pastYearsData = data_for_a_certain_y(2, data)
+    p_w, p_l, p_d = best_country(c, pastYearsData)
+    df = new_df(c, p_w, p_l, p_d, df, True, 2)
+    lst = list(df.iloc[0])
+#    print(len(df))
+    pts_A = []
+    pts_N = []
+    for idx in range(0, len(df)):
+        lst = list(df.iloc[idx])
+#        print(lst[0], "\tAVERAGE", lst[5]*3+lst[7], "\tNOW", lst[9]*3+lst[11])
+#        print(lst[8], lst[12])
+        if lst[8] != 0:
+            pts_A.append(((lst[5]*3+lst[7])/lst[8])*100)
+        else:
+            pts_A.append(1)
+        if lst[12] != 0:
+#            print(lst[12], ((lst[9]*3+lst[11])/lst[12])*100)
+            pts_N.append(((lst[9]*3+lst[11])/lst[12])*100)
+        else:
+            pts_N.append(0)
+    df["Last pts"] = pts_A
+    df["Now pts"] = pts_N
+    print(len(pts_A))
+    stats = []
+    s = []
+    for idx, i in enumerate(pts_A):
+#        print(i, pts_N[idx] / i)
+        if i != 0:
+            stats.append(pts_N[idx] / i)
+            s.append('Cold' if stats[idx] < 0.9 else 'Hot' if stats[idx] > 1.1 else 'Regular')
+        else:
+            stats.append(None)
+            s.append(None)
+    df["strike"] = s
+#    print(df)
+    worldCup = []
+    for i in GROUP:
+        worldCup.append(df.loc[df['TEAM'].isin(i)])
+    worldCup = pd.concat(worldCup)
+    print(worldCup)
+
+
+
+#    print(rank("World class", "Bad"))
+
+
+if __name__ == "__main__":
+    main()
